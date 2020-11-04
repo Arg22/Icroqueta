b63600ab1f0f8d95aab9702a5bbd770207f1e638
Alipinha <aliciarguzman22@gmail.com>
2020-11-04 19:54:37 +0100

    Prueba

--- app/src/main/java/com/example/icroqueta/Registro.java
+++ app/src/main/java/com/example/icroqueta/Registro.java
@@ -1,8 +1,13 @@
 package com.example.icroqueta;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class Registro extends AppCompatActivity {
 
@@ -10,5 +15,16 @@ public class Registro extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
+    }
+
+
+
+
+    public void openMain(View view) {
+        Intent intent= new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
+    public void onRadioButtonClicked(View view) {
     }
 }
