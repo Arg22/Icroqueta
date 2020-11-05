bd101b98fd535074cade3040ebbf433c728af4b2
Alipinha <aliciarguzman22@gmail.com>
2020-11-05 12:58:17 +0100

    Prueba

--- app/src/main/java/com/example/icroqueta/MainActivity.java
+++ app/src/main/java/com/example/icroqueta/MainActivity.java
@@ -4,7 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,5 +17,33 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    @Override
+public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu,menu);
+        return true;
+}
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        //Intent intent=     // intent= new Intent(this, LoginActivity.class);
+        switch (item.getItemId()){
+            case R.id.myAccount:
+                Toast.makeText(getBaseContext(),"micuenta",Toast.LENGTH_LONG).show();
+                break;
+            case R.id.myOptions:
+                Toast.makeText(getBaseContext(),"misOpciones",Toast.LENGTH_LONG).show();
+                break;
+            case R.id.myHistory:
+                Toast.makeText(getBaseContext(),"myHistory",Toast.LENGTH_LONG).show();
+                break;
+            case R.id.myOrders:
+                Toast.makeText(getBaseContext(),"myOrders",Toast.LENGTH_LONG).show();
+                break;
 
+        }
+
+
+
+       // startActivity(intent);
+        return super.onOptionsItemSelected(item);
+    }
 }
