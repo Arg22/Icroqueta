78f432b116cb9d56d8793c1b21c4d2978f81b987
Alipinha <aliciarguzman22@gmail.com>
2020-11-13 13:44:34 +0100

    Again

--- app/src/main/java/com/example/icroqueta/MainActivity.java
+++ app/src/main/java/com/example/icroqueta/MainActivity.java
@@ -36,7 +36,7 @@ public class MainActivity extends MenuBar {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
 
-        mAppBarConfiguration = new AppBarConfiguration.Builder().build();
+        mAppBarConfiguration = new AppBarConfiguration.Builder(R.id.nav_home).build();
 
 
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
