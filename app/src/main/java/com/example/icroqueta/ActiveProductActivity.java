6ffd38fae3073366c2f0527a728ee5a1dc6d7f72
Alipinha <aliciarguzman22@gmail.com>
2020-12-01 00:17:40 +0100

    Limpieza de código

--- app/src/main/java/com/example/icroqueta/ActiveProductActivity.java
+++ app/src/main/java/com/example/icroqueta/ActiveProductActivity.java
@@ -2,11 +2,21 @@ package com.example.icroqueta;
 
 import android.os.Bundle;
 
+import java.util.Objects;
+
 public class ActiveProductActivity extends MenuBar {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_active_product);
+
+        Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true); //Botón home
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return super.onSupportNavigateUp();
     }
 }
